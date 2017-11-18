afc1a654a4d25ffb7130531faa1ff7254c8509b5
khlyy <mohamedkhly@gmail.com>
2017-11-18 11:01:03 +0200

    First knowledge base the agent can push the rock with some bugs

--- EscapeR2D2.pl
+++ EscapeR2D2.pl
@@ -3,58 +3,53 @@ wall(cell(I, J)) :-
     I < 0; I > 2; J < 0; J > 2.
 
 teleportal(cell(2, 2)).
+obstacle(cell(10, 10)).
+%obstacle(cell(0, 1)).
+%obstacle(cell(0, 2)).
 
-obstacle(cell(0, 1)).
-obstacle(cell(0, 2)).
-
-rock_location(cell(1, 0), s0).
-pressure_pad(cell(2, 0)).
-
-up_cell(cell(I1, J1), cell(I2, J1)) :-  % First cell is over the second cell.
-  I2 is I1 + 1.
-
-down_cell(cell(I1, J1), cell(I2, J1)) :-
-  I2 is I1 - 1.
-
-right_cell(cell(I1, J1), cell(I1, J2)) :-
-  J2 is J1 - 1.
-
-left_cell(cell(I1, J1), cell(I1, J2)) :-
-  J2 is J1 + 1.
+rock_location(cell(1, 1), s0).
+pressure_pad(cell(0, 2)).
 
 at_location(cell(0, 0), s0).
-at_location(cell(0, 0), s0).
 
-valid_move(cell(I, J)) :- % TODO:: add teleportal if the rocks are not on the pads,
-  \+ wall(cell(I, J)), \+ obstacle(cell(I, J)).
 
-move_to_adjacent(Action, Curr_cell, New_cell) :-
-  Action = up, up_cell(Curr_cell, New_cell).
+oposite(up, down).
+oposite(down, up).
+oposite(left, right).
+oposite(right, left).
 
-move_to_adjacent(Action, Curr_cell, New_cell) :-
-  Action = down, down_cell(Curr_cell, New_cell).
+adjacent(cell(I1, J1), cell(I2, J1), up) :- I2 is I1 + 1.
+adjacent(cell(I1, J1), cell(I2, J1), down) :- I2 is I1 - 1.
+adjacent(cell(I1, J1), cell(I1, J2), left) :- J2 is J1 + 1.
+adjacent(cell(I1, J1), cell(I1, J2), right) :- J2 is J1 - 1.
 
-move_to_adjacent(Action, Curr_cell, New_cell) :-
-  Action = right, right_cell(Curr_cell, New_cell).
-
-move_to_adjacent(Action, Curr_cell, New_cell) :-
-  Action = left, left_cell(Curr_cell, New_cell).
+% teleportal(X), call_with_depth_limit(at_location(X, Y), 10, R).
+valid_cell(Cell) :- % TODO:: add teleportal if the rocks are not on the pads,
+  \+ wall(Cell), \+ obstacle(Cell).
 
 at_location(Curr_cell, result(Action, State)) :-
-  move_to_adjacent(Action, Curr_cell, New_cell), valid_move(Curr_cell), valid_move(New_cell), at_location(New_cell, State).
+  teleportal(Curr_cell), pressure_pad(Pad), rock_location(Pad, State), adjacent(Curr_cell, New_cell,
+   Direction), Action = move(Direction), valid_cell(Curr_cell), valid_cell(New_cell), at_location(New_cell, State),
+  \+ rock_location(Curr_cell, State).
 
-rock_location(cell(I, J), result(Action, State)):-
-  A = push_rock_up, up_cell(cell(I, J), New_cell), rock_location(New_cell, State), up_cell(New_cell, Agent_cell),
-  at_location(Agent_cell, State), valid_move(cell(I,J)), \+ rock_location(cell(I,J), State).
+at_location(Curr_cell, result(Action, State)) :-
+  \+ teleportal(Curr_cell), adjacent(Curr_cell, New_cell, Direction), Action = move(Direction), valid_cell(Curr_cell),
+   valid_cell(New_cell), at_location(New_cell, State),
+  \+ rock_location(Curr_cell, State).
 
-rock_location(cell(I, J), result(Action, State)):-
-  A = push_rock_down, down_cell(cell(I, J), New_cell), rock_location(New_cell, State), down_cell(New_cell, Agent_cell),
-  at_location(Agent_cell, State), valid_move(cell(I,J)), \+ rock_location(cell(I,J), State).
+at_location(Curr_cell, result(Action, State)) :-
+  \+ teleportal(Curr_cell), adjacent(Curr_cell, Agent_cell, Direction), at_location(Agent_cell, State),
+  rock_location(Curr_cell, State), oposite(Direction, Oposite_direction), Action = push(Direction), valid_cell(Curr_cell),
+  adjacent(Curr_cell, Rock_cell, Oposite_direction), valid_cell(Rock_cell).
 
-rock_location(cell(I, J), result(Action, State)):-
-  A = push_rock_left, left_cell(cell(I, J), New_cell), rock_location(New_cell, State), left_cell(New_cell, Agent_cell),
-  at_location(Agent_cell, State), valid_move(cell(I,J)), \+ rock_location(cell(I,J), State).
+% at_location(Curr_cell, result(Action, State)) :-
+%   adjacent(Curr_cell, New_cell, Direction), Action = move(Direction), valid_cell(Curr_cell), valid_cell(New_cell), at_location(New_cell, State).
 
-rock_location(cell(I, J), result(Action, State)):-
-  A = push_rock_right, up_cell(cell(I, J), New_cell), rock_location(New_cell, State), right_cell(New_cell, Agent_cell),
-  at_location(Agent_cell, State), valid_move(cell(I,J)), \+ rock_location(cell(I,J), State).
+rock_location(Curr_cell, result(Action, State)) :-
+  adjacent(Curr_cell, New_cell, Direction), adjacent(New_cell, Agent_cell, Direction), Action = push(Direction),
+  rock_location(New_cell, State), \+ rock_location(Curr_cell, State), at_location(Agent_cell, State), valid_cell(Curr_cell).
+
+rock_location(Curr_cell, result(Action, State)) :-
+  rock_location(Curr_cell, State),
+  \+ (adjacent(Curr_cell, Agent_cell, Direction), at_location(Agent_cell, State), Action = push(Direction), oposite(Direction, Oposite_direction),
+  adjacent(Curr_cell, New_cell, Oposite_direction), \+ rock_location(New_cell, State), valid_cell(New_cell)).
